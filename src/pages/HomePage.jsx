d96dd31f4d55a760816fbccd954209cef045ac24
DaoTuanCuong <cuongdaotuan@gmail.com>
2021-01-03 22:51:37 +0700

    set active items

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch, shallowEqual } from 'react-redux';
 import HobbyList from '../components/Home/HobbyList';
 import { addNewHobby, setActiveHobby } from '../actions/hobby';
 
+
 HomePage.propTypes = {
 
 };
@@ -13,13 +14,45 @@ const randomNumber = () => {
 
 function HomePage(props) {
   const hobbyList =  useSelector(state => state.hobby.list);
+  const activeId = useSelector(state => state.hobby.activeId);
+
+  // const hobbyState = useSelector(state =>({
+  //   list : state.hobby.list,
+  //   activeId : state.hobby.activeId
+  // }))
+
+  const dispatch = useDispatch();
+
   console.log("hobbyList", hobbyList);
+
+  const handleAddHobbyClick = () => {
+    const newId = randomNumber()
+    const newHobby = {
+      id: newId,
+      title: `Hobby ${newId}`,
+    }
+
+    const action = addNewHobby(newHobby);
+    dispatch(action)
+  }
+
+  const handleHobbyClick = (hobby) => {
+    const action = setActiveHobby(hobby);
+    dispatch(action);
+
+  }
 
   return (
     <div className="home-page">
       <h1>REDUX HOOKS - Home Page</h1>
 
-     
+      <button onClick={handleAddHobbyClick}>Random hobby</button>
+
+      <HobbyList 
+        hobbyList={hobbyList} 
+        activeId={activeId}
+        onHobbyClick={handleHobbyClick}
+      />
     </div>
   );
 }
